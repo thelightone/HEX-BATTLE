4cdec8dc31faee9e93fb89e36bc683c9d41adb05
Andrey <an.i.nesterov@gmail.com>
2023-08-26 09:44:50 +0300

    -

---
+++ Assets/CameraRaycast.cs
@@ -0,0 +1,68 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Burst.CompilerServices;
+using UnityEngine;
+
+public class CameraRaycast : MonoBehaviour
+{
+    private Camera _mainCamera;
+    private HexTile _targetHex;
+    private UnitController _targetUnit;
+    private Vector3 _mousePos;
+
+    private void Start()
+    {
+        _mainCamera = GetComponent<Camera>();
+    }
+
+    void Update()
+    {
+
+        RaycastHit hit;
+        var plane = new Plane(Vector3.up, Vector3.zero);
+        _mousePos = Input.mousePosition;
+
+        var ray = _mainCamera.ScreenPointToRay(_mousePos);
+
+        if (Physics.Raycast(ray, out hit))
+        {
+            Transform objectHit = hit.transform;
+            var parent = objectHit.parent;
+
+            if ( parent.GetComponent<UnitController>())
+            {
+                HighlightUnit(parent);
+            }
+            else
+            {
+                TileManager.Instance.DisLightUnit();
+
+                if (TileManager.Instance.activeUnit != null && parent.GetComponent<HexTile>())
+                {
+                    _targetHex = parent.GetComponent<HexTile>();
+                    _targetHex.Highlight();
+                    if (Input.GetMouseButtonUp(0))
+                    {
+                        _targetHex.Select();
+                    }
+                }
+            }
+
+        }
+    }
+
+    private void HighlightUnit(Transform objectHit)
+    {
+
+        _targetUnit = objectHit.GetComponent<UnitController>();
+
+        TileManager.Instance.LightUnit(_targetUnit);
+
+        if (Input.GetMouseButtonUp(0))
+        {
+            TileManager.Instance.ChooseUnit(_targetUnit);
+        }
+
+
+    }
+}
---
+++ Assets/HexTile.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HexTile : MonoBehaviour
+{
+    public Vector2Int offsetCoord;
+    public Vector3Int cubeCoord;
+    public List<HexTile> neigh;
+    public bool busy;
+
+
+
+    public void OnDrawGizmosSelected()
+    {
+        foreach (var neig in neigh)
+        {
+            Gizmos.color = Color.blue;
+            Gizmos.DrawSphere(transform.position, 0.1f);
+            Gizmos.color = Color.white;
+            Gizmos.DrawLine(transform.position, neig.transform.position);
+        }
+    }
+
+    public void Highlight()
+    {
+        if ( !TileManager.Instance.activeUnit.isMoving)
+        {
+            if (!busy && TileManager.Instance.activeUnit.currentRange.Contains(this))
+                TileManager.Instance.Highlight(this);
+            else
+                TileManager.Instance.Busy(this);
+        }
+        }
+
+        public void Select()
+        {
+            TileManager.Instance.Select(this);
+        }
+    }
